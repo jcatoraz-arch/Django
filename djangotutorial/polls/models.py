512723fd296cb8443805b9521714b1dd63ade282
“pepito <jcatoraz@alumno.huergo.edu.ar>
2026-09-03 14:05:17 -0300

    termine la parte dos pero se me borro la vitacora, ahora veo como agregarla de nuevo

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# esto seria el cuestionario 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+    
+# aca estoy creando las preguntas que van a estar relacionadas por un foreignkey con las opciones
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
